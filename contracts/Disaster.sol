b9c0e2fd6a6a798718d9ce4be4cef768113f5d4a
Tommy Cox <tommy.cox@protonmail.com>
2019-09-07 19:50:22 -0400

    Track how much has been contributed by a user

--- contracts/Disaster.sol
+++ contracts/Disaster.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/payment/PullPayment.sol";
+import "@openzeppelin/contracts/payment/escrow/Escrow.sol";
 
-contract Disaster is PullPayment {
+contract Disaster is Escrow {
     string public name;
     string public description;
     string public location;
-    address[] public contributerArray;
 
-    mapping (address => uint) contributers;
+    mapping (address => uint) contributionAmount;
 
     constructor(
         string memory _name,
@@ -18,5 +17,13 @@ contract Disaster is PullPayment {
         name = _name;
         description = _description;
         location = _location;
+    }
+
+    /**
+     * @dev Track how much has been contributed by a user.
+     */
+    function deposit (address payee) public payable {
+        contributionAmount[msg.sender] += msg.value;
+        super.deposit(payee);
     }
 }
