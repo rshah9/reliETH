28ebeb485691c6329acbc74b6435c44716000dac
Rama Shah <ramashah@Ramas-MacBook.local>
2019-09-06 23:25:45 -0400

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <img src={logo} className="App-logo" alt="logo" />
         <h1> reliETH </h1>
         <h4> ethBoston Hackathon Disaster Relief</h4>
       </header>
