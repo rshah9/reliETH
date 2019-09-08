ee8b79b0861a31c7aea1894c99373bbf45e2ff78
Tommy Cox <tommy.cox@protonmail.com>
2019-09-07 22:18:19 -0400

    Deploy two Disasters on migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,20 @@
 const Relieth = artifacts.require("Relieth");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   deployer.deploy(Relieth);
+  const relieth = await Relieth.deployed();
+
+  relieth.createDisaster(
+    "Hurricane Dorian",
+    "The fourth named hurricane of the 2019 season, Hurricane Dorian has tied the record for the most powerful hurricane to make landfall in the Bahamas. Donate to support the cause.",
+    "Alabama",
+    web3.utils.randomHex(32)
+  );
+
+  relieth.createDisaster(
+    "Amazon Rainforest Fire",
+    "Brazil’s Amazon rainforest fires have caused global concern. And yet it could get even worse as the country’s fire season is just getting started. Donate to support the cause.",
+    "Brazil",
+    web3.utils.randomHex(32)
+  );
 };
