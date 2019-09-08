72fa7e6ac56c7cb648bf8ce183cd8652dade1408
Tommy Cox <tommy.cox@protonmail.com>
2019-09-07 23:38:43 -0400

    Need to await creation of disasters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,20 @@
 const Relieth = artifacts.require("Relieth");
 
-module.exports = async function(deployer) {
+module.exports = async (deployer) => {
   await deployer.deploy(Relieth);
   const relieth = await Relieth.deployed();
 
-  relieth.createDisaster(
+  await relieth.createDisaster(
     "Hurricane Dorian",
     "The fourth named hurricane of the 2019 season, Hurricane Dorian has tied the record for the most powerful hurricane to make landfall in the Bahamas. Donate to support the cause.",
     "Alabama",
-    web3.utils.randomHex(32)
+    web3.utils.randomHex(20)
   );
 
-  relieth.createDisaster(
+  await relieth.createDisaster(
     "Amazon Rainforest Fire",
     "Brazil’s Amazon rainforest fires have caused global concern. And yet it could get even worse as the country’s fire season is just getting started. Donate to support the cause.",
     "Brazil",
-    web3.utils.randomHex(32)
+    web3.utils.randomHex(20)
   );
 };
