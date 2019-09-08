4c5aad2d27aecc3bf002d7e082e84b46e6d598fc
Will <williamlopezc@gmail.com>
2019-09-08 02:28:36 -0400

    connected dashboard with contracts

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './header';
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
@@ -7,43 +7,156 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-
-import web3 from '../web3';
 import contract from "truffle-contract";
-
 import ReliethABI from "../abis/Relieth";
 import DisasterABI from "../abis/Disaster";
-
-const Relieth = contract(ReliethABI);
-const Disaster = contract(DisasterABI);
-
-Relieth.setProvider(web3.currentProvider);
-Disaster.setProvider(web3.currentProvider);
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            disasters: []
+            disasters: [],
+            disasterComponents: [],
+            web3: '',
+            relieth: '',
+            disaster: '',
+            imageOne: 'https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/AP-19248552550369-1567803265.jpg',
+            imageTwo: 'https://info.umkc.edu/unews/wp-content/uploads/2019/09/566ee9cf1900002300789c85-620x400.jpg',
         };
     }
 
-    async componentDidMount() {
-        const relieth = await Relieth.deployed();
-        const disasterAddresses = await relieth.getDisasters();
-        
-        disasterAddresses.forEach(async address => {
-            const disaster = await Disaster.at(address);
-            this.state.disasters.push({
-                name: await disaster.name(),
-                description: await disaster.description(),
-                location: await disaster.location()
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.web3 !== prevProps.web3) {
+            this.setState({web3: this.props.web3});
+        }
+        if (this.state.web3 !== prevState.web3) {
+                const Relieth = contract(ReliethABI);
+                const Disaster = contract(DisasterABI);
+                Relieth.setProvider(this.state.web3.currentProvider);
+                Disaster.setProvider(this.state.web3.currentProvider);
+                Relieth.deployed().then((value) => {
+                    this.setState({relieth: value, disaster: Disaster});
+                });
+        }
+        if (this.state.relieth !== prevState.relieth) {
+            const disasters = [];
+            const disasterComponents = [];
+            this.state.relieth.getDisasters().then((disasterAddresses) => {
+                disasterAddresses.forEach(async address => {
+                    const disaster = await this.state.disaster.at(address);
+                    const disasterName = await disaster.name();
+                    const disasterDescription = await disaster.description();
+                    const disasterLocation = await disaster.location();
+                    disasters.push({
+                        name: disasterName,
+                        description: disasterDescription,
+                        location: disasterLocation,
+                    });
+                    disasterComponents.push(
+                        <Card style={{ marginBottom: '30px' }}>
+                        <Grid container spacing={2}>
+                            <Grid style={{ marginBottom: '-8px' }} item>
+                                <img alt="disaster"
+                                     style={{ height: '200px', width: '200px' }}
+                                     src={(disasterName === 'Hurricane Dorian') ? (this.state.imageOne) : (this.state.imageTwo) }
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm container>
+                                <Grid item xs container direction="column" spacing={2}>
+                                    <Grid item xs>
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {disasterName}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                {disasterDescription}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button variant="contained" color="primary" style={{ height:'50px', width:'200px', fontWeight:'bold', fontSize: '20px'}}>
+                                                <a style={{ textDecoration : "none", color: 'white'}} href="https://app.squarelink.com/tx?client_id=3c6f59c08f0c96290c38&network=ropsten&value=10000000000000000&to=0x824F8C464Fd43EF15aE87DB86ea30Ff91FAb2ac3">DONATE </a>
+                                            </Button>
+                                        </CardActions>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                    )
+                });
+                this.setState({disasters, disasterComponents});
             });
-        });
-        console.log(this.state.disasters);
+        }
     }
 
     render() {
+        const defaultComponents = (
+            <>
+                <Card style={{ marginBottom: '30px' }}>
+                    <Grid container spacing={2}>
+                        <Grid style={{ marginBottom: '-8px' }} item>
+                            <img alt="disaster"
+                                 style={{ height: '230px', width: '230px' }}
+                                 src={"https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/AP-19248552550369-1567803265.jpg"}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm container>
+                            <Grid item xs container direction="column" spacing={2}>
+                                <Grid item xs>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Hurricane Dorian
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Hurricane Dorian, the fourth named hurricane of the 2019 season,
+                                            tied the record for the most powerful hurricane to make landfall
+                                            in the Bahamas. Donate to support the cause.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button variant="contained" color="primary" style={{ height:'50px', width:'200px', fontWeight:'bold', fontSize: '20px'}}>
+                                            <a style={{ textDecoration : "none", color: 'white'}} href="https://app.squarelink.com/tx?client_id=3c6f59c08f0c96290c38&network=ropsten&value=10000000000000000&to=0x824F8C464Fd43EF15aE87DB86ea30Ff91FAb2ac3">DONATE </a>
+                                        </Button>
+                                    </CardActions>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Card>
+                <Card style={{ marginBottom: '30px' }}>
+                    <Grid container spacing={2}>
+                        <Grid style={{ marginBottom: '-8px' }} item>
+                            <img alt="disaster"
+                                 style={{ height: '230px', width: '230px' }}
+                                 src={"https://info.umkc.edu/unews/wp-content/uploads/2019/09/566ee9cf1900002300789c85-620x400.jpg"}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm container>
+                            <Grid item xs container direction="column" spacing={2}>
+                                <Grid item xs>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Amazon Rainforest Fire
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Brazil’s Amazon rainforest fires have caused global concern.
+                                            And yet it could get even worse as the country’s fire season is
+                                            just getting started. Donate to support the cause.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button variant="contained" color="primary" style={{ height:'50px', width:'200px', fontWeight:'bold', fontSize: '20px'}}>
+                                            <a style={{ textDecoration : "none", color: 'white'}} href="https://app.squarelink.com/tx?client_id=3c6f59c08f0c96290c38&network=ropsten&value=10000000000000000&to=0x824F8C464Fd43EF15aE87DB86ea30Ff91FAb2ac3">DONATE </a>
+                                        </Button>
+                                    </CardActions>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </>
+        );
+        const renderComponents = (this.state.disasterComponents.length !== 0) ? this.state.disasterComponents : defaultComponents;
         return (
             <>
                 <Header title={'Dashboard'}/>
@@ -55,68 +168,7 @@ class Dashboard extends React.Component {
                                     <Typography  variant="h5" component="h3" style={{textAlign: 'center', fontWeight: "bold", paddingBottom: '10px' }}>
                                         Current Disaster Relief Events
                                     </Typography>
-                                    <Card style={{ marginBottom: '30px' }}>
-                                        <Grid container spacing={2}>
-                                            <Grid style={{ marginBottom: '-8px' }} item>
-                                                <img alt="disaster"
-                                                    style={{ height: '200px', width: '200px' }}
-                                                    src={"https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/AP-19248552550369-1567803265.jpg"}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12} sm container>
-                                                <Grid item xs container direction="column" spacing={2}>
-                                                    <Grid item xs>
-                                                        <CardContent>
-                                                            <Typography gutterBottom variant="h5" component="h2">
-                                                                Hurricane Dorian
-                                                            </Typography>
-                                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                                Hurricane Dorian, the fourth named hurricane of the 2019 season,
-                                                                tied the record for the most powerful hurricane to make landfall
-                                                                in the Bahamas. Donate to support the cause.
-                                                            </Typography>
-                                                        </CardContent>
-                                                        <CardActions>
-                                                            <Button variant="contained" color="primary" style={{ height:'50px', width:'200px', fontWeight:'bold', fontSize: '20px'}}>
-                                                                <a style={{ textDecoration : "none", color: 'white'}} href="https://app.squarelink.com/tx?client_id=3c6f59c08f0c96290c38&network=ropsten&value=10000000000000000&to=0x824F8C464Fd43EF15aE87DB86ea30Ff91FAb2ac3">DONATE </a>
-                                                            </Button>
-                                                        </CardActions>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Card>
-                                    <Card style={{ marginBottom: '30px' }}>
-                                        <Grid container spacing={2}>
-                                            <Grid style={{ marginBottom: '-8px' }} item>
-                                                <img alt="disaster"
-                                                    style={{ height: '200px', width: '200px' }}
-                                                    src={"https://info.umkc.edu/unews/wp-content/uploads/2019/09/566ee9cf1900002300789c85-620x400.jpg"}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12} sm container>
-                                                <Grid item xs container direction="column" spacing={2}>
-                                                    <Grid item xs>
-                                                        <CardContent>
-                                                            <Typography gutterBottom variant="h5" component="h2">
-                                                                Amazon Rainforest Fire
-                                                            </Typography>
-                                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                                Brazil’s Amazon rainforest fires have caused global concern.
-                                                                And yet it could get even worse as the country’s fire season is
-                                                                just getting started. Donate to support the cause.
-                                                            </Typography>
-                                                        </CardContent>
-                                                        <CardActions>
-                                                            <Button variant="contained" color="primary" style={{ height:'50px', width:'200px', fontWeight:'bold', fontSize: '20px'}}>
-                                                                <a style={{ textDecoration : "none", color: 'white'}} href="https://app.squarelink.com/tx?client_id=3c6f59c08f0c96290c38&network=ropsten&value=10000000000000000&to=0x824F8C464Fd43EF15aE87DB86ea30Ff91FAb2ac3">DONATE </a>
-                                                            </Button>
-                                                        </CardActions>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </Card>
+                                    {renderComponents}
                                 </Grid>
                             </Grid>
                         </Container>
