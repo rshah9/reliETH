582827cf4e46549a60da87113f032fa2fe79dd40
Aditya Bharadwaj <aditya.bharadwaj@panerabread.com>
2019-09-08 05:22:23 -0400

    Rama: adding user name to squarelink

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,7 +10,7 @@ class Home extends React.Component {
                     <h1> reliETH </h1>
                     <h4> Welcome to the ethBoston Hackathon Disaster Relief page</h4>
                     <h4> Please login to continue</h4>
-                        <a href="https://app.squarelink.com/authorize?client_id=3c6f59c08f0c96290c38&scope=[user:email,wallets:read]&redirect_uri=https://relieth.rshah9.now.sh/dashboard&response_type=token">
+                        <a href="https://app.squarelink.com/authorize?client_id=3c6f59c08f0c96290c38&scope=[user:name,user:email,wallets:read]&redirect_uri=https://relieth.rshah9.now.sh/dashboard&response_type=token">
                         <img alt="squarelink-login" src="https://squarelink.com/img/sign-tx.svg" width="400"/></a>
                 </div>
             </div>
